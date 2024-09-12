c59afb02c548ba303c6f99adb8af0783ab3e96e9
Juan camilo delgado ceballos <juan.delgado-c@uniminuto.edu.co>
2024-09-12 01:18:59 +0000

    programa de preguntar tu edad y nombre

---
+++ SemanaUno/lab-1.py
@@ -0,0 +1,7 @@
+def get_average_and_largest():
+    name = input("ingrese su nombre: ")
+    age = int(input("ingrese su edad: "))
+
+    print(f"me llamo {name} y tengo {age}")
+get_average_and_largest()
+   
