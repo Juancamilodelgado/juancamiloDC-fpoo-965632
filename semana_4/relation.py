2c1849feedd71331fd7664051d54094129af045e
Juan camilo delgado ceballos <juan.delgado-c@uniminuto.edu.co>
2024-10-01 02:26:59 +0000

    semana_4

---
+++ semana_4/relation.py
@@ -0,0 +1,47 @@
+class author:
+    def __init__(self,name):
+        self.name = name
+        self.books = []
+    
+    def add_book(self,book: 'Book'):
+        self.books
+
+     
+
+
+
+
+
+class books:
+    def add_book(self,title: str,pages:int,author):
+        self.title = title
+        self.pages = pages
+        self.author = author
+    
+    def __str__(self):
+        return f"libro: {self.title}"
+
+
+
+
+class Library:
+    def __init__(self):
+        self.book = []
+
+
+
+ author_1= Author(name='gabriel garcia maquez')
+
+ book_1 = Book('cien años de soledad',473 author_1)
+
+
+
+
+
+        
+
+
+
+    
+
+      
